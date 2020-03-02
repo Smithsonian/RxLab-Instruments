f286ff3559953efdcd865f1ef13e3bf2eeb87535
John Garrett <john.garrett@astro.ox.ac.uk>
2020-03-02 11:54:57 -0500

    Add error in package name

--- labinstruments/__init__.py
+++ labinstruments/__init__.py
@@ -4,9 +4,9 @@ Control instruments in the receiver lab over Ethernet using SCPI commands.
 
 """
 
-from labinstrument.hittite import Hittite, SignalGenerator
-# from labinstrument.keithley import Keithley2280
-# from labinstrument.microlambda import YigFilter
+from labinstruments.hittite import Hittite, SignalGenerator
+# from labinstruments.keithley import Keithley2280
+# from labinstruments.microlambda import YigFilter
 
 __author__ = "John Garrett"
 __version__ = "0.0.1"
