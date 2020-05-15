62dba465aea2a9d4c2010c3cbc7ed59ffa00dbec
John Garrett <john.garrett@astro.ox.ac.uk>
2020-05-14 23:13:53 -0400

    siglent: add method to read rms voltage

--- labinstruments/siglent.py
+++ labinstruments/siglent.py
@@ -1,29 +1,117 @@
-import socket
+"""Class to read data from Siglent oscilloscopes.
+
+Supported models:
+
+    ...
+
+Siglent SCPI instructions: 
+
+    "Digital Oscilloscopes, Programming Guide, PG01-E02C"
+
+"""
+
 import os
+import socket
 import sys
 import time
 
+
 class Siglent:
+    """Class to read data from Siglent oscilloscopes.
 
-    def __init__(self):
+    Note:
 
-        self.skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.skt.connect(('192.168.0.10', 5025))
+        This has only been tested with an SDS1104X-E oscilloscope.
+
+    Args:
+        ip_address (string): IP address of the Siglent oscilloscope, e.g.,
+            ``ip_address='192.168.0.10'``
+        port (int, optional, default is 5025): the port set for Ethernet
+            communication
+
+    """
+
+    def __init__(self, ip_address='192.168.0.10', port=5025):
+
+        # Create socket
+        try:
+            self._skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as e:
+            print('Error creating socket: %s' % e)
+            sys.exit(1)
+
+        # Connect to oscilloscope
+        try:
+            self._skt.connect((ip_address, port))
+        except socket.gaierror as e:
+            print('Address-related error connecting to instrument: %s' % e)
+            sys.exit(1)
+        except socket.error as e:
+            print('Error connecting to socket on instrument: %s' % e)
+            sys.exit(1)
 
     def _send(self, msg):
+        """Send command to instrument.
+
+        Args:
+            msg (string): command to send
+
+        """
 
         msg = msg + '\n'
-        self.skt.send(msg.encode('ASCII'))
+        self._skt.send(msg.encode('ASCII'))
 
     def _receive(self):
+        """Receive message from instrument.
 
-        msg = self.skt.recv(1024).decode('ASCII')
+        Returns:
+            string: output from instrument
+
+        """
+
+        msg = self._skt.recv(1024).decode('ASCII')
         return msg.strip()
+        
+    def close(self):
+        """Close connection to instrument."""
 
-    def get_idn(self):
+        self._skt.close()
+
+    def get_id(self):
+        """Get identity of oscilloscope."""
 
         self._send('*IDN?')
         return self._receive()
+
+    def reset(self):
+        """Reset oscilloscope."""
+
+        self._send("*RST")
+
+    def measure_rms_voltage(self, channel=1):
+        """Measure RMS voltage.
+
+        Args:
+            units (str): units for voltage measurement
+
+        Returns:
+            float: DC voltage
+
+        """
+
+        # Set mode
+        msg = 'PACU RMS,C{}'
+        self._send(msg.format(channel))
+
+        # Read RMS voltage
+        msg = "C{}:PAVA? RMS"
+        self._send(msg.format(channel))
+        value = self._receive()
+
+        print(value)
+
+        return value.strip()[-1]
+
 
 if __name__ == "__main__":
 
